898f64bd2edefb7eeb638a049ee0e72cc41bd27c
Patrick Denning <patrickdenning@Patricks-MBP.broadband>
2024-02-14 06:59:55 +0000

    Reset initial commit

---
+++ src/lists/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from .models import Item, List
+
+
+def home_page(request):
+    return render(request, "home.html")
+
+
+def view_list(request, list_id):
+    list = List.objects.get(id=list_id)
+    return render(request, "list.html", {"list": list})
+
+
+def new_list(request):
+    list = List.objects.create()
+    Item.objects.create(text=request.POST["item_text"], list=list)
+    return redirect(f"/lists/{list.id}/")
+
+
+def add_item(request, list_id):
+    list = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST["item_text"], list=list)
+    return redirect(f"/lists/{list.id}/")
